00e66f3bd7b270799a203fe7be6202b481881c27
Prince Goswami <princegoswami.space@gmail.com>
2025-04-18 18:40:06 +0530

    added exception handling

--- Action_Suggester/views.py
+++ Action_Suggester/views.py
@@ -15,7 +15,10 @@ def suggestion(request):
     if not request.data.get("query"):
         return Response(f"not enough field {request.POST}", status=status.HTTP_400_BAD_REQUEST)
     # RUN THE QUERY
-    return Response(tone_and_intent_analyzer(request.data.get("query")), status=status.HTTP_200_OK)
+    response_data = tone_and_intent_analyzer(request.data.get("query"))
+    if (response_data.get("status", True)):
+        return Response(f"some error occured", status=status.HTTP_400_BAD_REQUEST)
+    return Response(response_data, status=status.HTTP_200_OK)
 
 class APILogViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = QueryLog.objects.all().order_by('-created_at')
